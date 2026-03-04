96497bd21b111b06d36eb19c2ad437096713bbeb
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2026-03-04 19:56:37 -0300

    Time: 279 ms (62.61%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,13 @@
+SELECT
+    x,
+    y,
+    z,
+    (CASE
+        WHEN (ABS(x) + ABS(y) > ABS(z) 
+         AND ABS(x) + ABS(z) > ABS(y)
+         AND ABS(z) + ABS(y) > ABS(x)) THEN "Yes"
+         ELSE "No"
+         END
+        ) AS triangle
+FROM
+    Triangle
